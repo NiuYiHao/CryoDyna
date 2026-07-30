490a095f5d25cf33300d3826306d052b1daff18b
牛艺豪 <9617612+niu-yihao@user.noreply.gitee.com>
2026-07-30 15:37:19 +0800

    Run pose optimization on 1ake

--- test_pose_optimize.py
+++ test_pose_optimize.py
@@ -37,8 +37,8 @@ def batch_projection(gauss: Gaussian, rot_mats: torch.Tensor, line_grid:Grid, gm
     if mask_type == 'amplitude':
         amplitudes = amplitudes * gmm_mask[None, :]
     elif mask_type == 'sigma':
-        sigmas = sigmas * (4 - 3* mask_gmm)
-        amplitudes =  amplitudes / (4 - 3* mask_gmm)**3
+        sigmas = sigmas * (4 - 3* gmm_mask)
+        amplitudes =  amplitudes / (4 - 3* gmm_mask)**3
         
     sigmas = 2 * sigmas**2
 
@@ -68,44 +68,44 @@ from scipy.spatial.distance import cdist
 from cryodyna.utils.pdb_tools import bt_save_pdb, extract_sec_ids_merge_small_blocks, build_metagraph_knn_from_centroids ,get_kplus_neighbor_metanodes
 # work_dir = '10792/atom_10792_10811'
 # num_step = '0049_0045650'
-work_dir = '../../CryoDyna/10073/atom_10073_10087'
-num_step = '0079_0086800'
+work_dir = '/media/nyh/ce66dee3-f568-4265-9a9c-a9c9b34c3973/group2/data/cryodyna/1ake/atom_1ake'
+num_step = '0029_0023430'
 cfg = Config.fromfile(f'{work_dir}/config.py')
 meta = Polymer.from_pdb(cfg.dataset_attr.ref_pdb_path)
 ref_centers = torch.from_numpy(meta.coord).float()
 ref_amps = torch.from_numpy(meta.num_electron).float()
 ref_sigmas = torch.ones_like(ref_amps)
 num_pts = len(meta)
-# sec_ids, _ = extract_sec_ids_merge_small_blocks(cfg.dataset_attr.ref_pdb_path ,min_block_len=3)
-# meta_edge_index, centroids = build_metagraph_knn_from_centroids( pos=meta.coord,sec_ids=sec_ids,k=cfg.knn_num)
-# sec_ids = torch.from_numpy(sec_ids).long()
-# meta_edge_index = meta_edge_index.long()
-# centroid_distances = cdist(centroids, centroids)
-# edge_dist = torch.from_numpy(centroid_distances[meta_edge_index[0], meta_edge_index[1]]).float()
-# pe_vector = torch.from_numpy(np.array([centroids[sec_id] - ref_centers[i] for i,sec_id in enumerate(sec_ids)]))
-# meta_2_node_edge, meta_2_node_vector = get_kplus_neighbor_metanodes(ref_centers,sec_ids,centroids)# node_meta_dist = cdist(ref_centers,centroids)
-# # in_dim = cfg.dataset_attr.side_shape ** 2
-# in_dim = 128 ** 2
-# from projects.miscs import VAE
-# model = VAE(in_dim=in_dim, sec_ids = sec_ids, meta_edge_index=meta_edge_index, edge_dist=edge_dist,out_dim = num_pts * 3, pe_vector=pe_vector ,meta_2_node_edge = meta_2_node_edge,meta_2_node_vector=meta_2_node_vector,**cfg.model.model_cfg)
-# z_list = np.load(f'{work_dir}/{num_step}/z.npy')
-# z = torch.from_numpy(z_list)
-# device = 'cuda:0'
-# weights = torch.load(f'{work_dir}/{num_step}/ckpt.pt')
-# model.load_state_dict(weights['model'])
-# model.eval()
-# model.to(device)
-# batch_size = 500
-# z_batches = torch.split(z, batch_size)
-# pred_gmm = []
-# for i, sampled_z in enumerate(z_batches):
-#     with torch.no_grad():
-#         delta = model.eval_z(sampled_z.to(torch.float32).to(device))
-#     pred_struc = delta.reshape(-1, num_pts, 3) + ref_centers[None, :, :].to(device)
-#     pred_gmm.append(pred_struc.detach().cpu())
-# pred_gmm = torch.cat(pred_gmm, dim=0)
+sec_ids, _ = extract_sec_ids_merge_small_blocks(cfg.dataset_attr.ref_pdb_path ,min_block_len=3)
+meta_edge_index, centroids = build_metagraph_knn_from_centroids( pos=meta.coord,sec_ids=sec_ids,k=cfg.knn_num)
+sec_ids = torch.from_numpy(sec_ids).long()
+meta_edge_index = meta_edge_index.long()
+centroid_distances = cdist(centroids, centroids)
+edge_dist = torch.from_numpy(centroid_distances[meta_edge_index[0], meta_edge_index[1]]).float()
+pe_vector = torch.from_numpy(np.array([centroids[sec_id] - ref_centers[i] for i,sec_id in enumerate(sec_ids)]))
+meta_2_node_edge, meta_2_node_vector = get_kplus_neighbor_metanodes(ref_centers,sec_ids,centroids)# node_meta_dist = cdist(ref_centers,centroids)
+# in_dim = cfg.dataset_attr.side_shape ** 2
+in_dim = 128 ** 2
+from projects.miscs import VAE
+model = VAE(in_dim=in_dim, sec_ids = sec_ids, meta_edge_index=meta_edge_index, edge_dist=edge_dist,out_dim = num_pts * 3, pe_vector=pe_vector ,meta_2_node_edge = meta_2_node_edge,meta_2_node_vector=meta_2_node_vector,**cfg.model.model_cfg)
+z_list = np.load(f'{work_dir}/{num_step}/z.npy')
+z = torch.from_numpy(z_list)
+device = 'cuda:0'
+weights = torch.load(f'{work_dir}/{num_step}/ckpt.pt')
+model.load_state_dict(weights['model'],strict=False)
+model.eval()
+model.to(device)
+batch_size = 500
+z_batches = torch.split(z, batch_size)
+pred_gmm = []
+for i, sampled_z in enumerate(z_batches):
+    with torch.no_grad():
+        delta = model.eval_z(sampled_z.to(torch.float32).to(device))
+    pred_struc = delta.reshape(-1, num_pts, 3) + ref_centers[None, :, :].to(device)
+    pred_gmm.append(pred_struc.detach().cpu())
+pred_gmm = torch.cat(pred_gmm, dim=0)
 import numpy as np
-pred_gmm = np.load('test_pose/pred_gmm.npy')
+# pred_gmm = np.load('test_pose/pred_gmm.npy')
 
 import mrcfile
 import torch
@@ -131,10 +131,11 @@ def check_points_in_mask_compact(points, mask, Apix=1.0):
     result[valid] = mask[indices[valid, 0], indices[valid, 1], indices[valid, 2]]
     
     return result
-mrc_data = mrcfile.read('/lustre/grp/gyqlab/share/cryoem_particles/10073/10073_arm_mask.mrc')
-mrc_data = F.interpolate(torch.from_numpy(mrc_data).unsqueeze(0).unsqueeze(0), size=(128,128,128), mode='trilinear', align_corners=False).squeeze(0).squeeze(0)
-mask_bool = mrc_data >= 1e-6
-mask_gmm = check_points_in_mask_compact(meta.coord, mask_bool.numpy(), Apix=4.15625)
+# mrc_data = mrcfile.read('/lustre/grp/gyqlab/share/cryoem_particles/10073/10073_arm_mask.mrc')
+# mrc_data = F.interpolate(torch.from_numpy(mrc_data).unsqueeze(0).unsqueeze(0), size=(128,128,128), mode='trilinear', align_corners=False).squeeze(0).squeeze(0)
+# mask_bool = mrc_data >= 1e-6
+# mask_gmm = check_points_in_mask_compact(meta.coord, mask_bool.numpy(), Apix=4.15625)
+mask_gmm = np.ones(len(meta.coord), dtype=int)
 
 from cryodyna.utils.dataio import StarfileDataSet, StarfileDatasetConfig
 dataset = StarfileDataSet(
@@ -153,12 +154,13 @@ dataset = StarfileDataSet(
 import starfile
 from pathlib import Path
 particle_df = starfile.read(Path(cfg.dataset_attr.starfile_path))
-sel_particle_df = particle_df
+sel_particle_df = particle_df["particles"]
+# print(sel_particle_df.keys())
 sel_pose = np.column_stack((sel_particle_df["rlnAngleRot"],
                             sel_particle_df["rlnAngleTilt"],
                             sel_particle_df["rlnAnglePsi"],
-                            np.array(sel_particle_df["rlnOriginX"] * 1.4, dtype=np.float32),
-                            np.array(sel_particle_df["rlnOriginY"] * 1.4, dtype=np.float32)))
+                            np.array(sel_particle_df["rlnOriginXAngst"] , dtype=np.float32),
+                            np.array(sel_particle_df["rlnOriginYAngst"] , dtype=np.float32)))
 
 def euler2rotmat_relion(poses):
     """
@@ -248,19 +250,21 @@ def _shared_projection(gmm, rot_mats, ref_sigmas,ref_amps,grid,mask_gmm):
     pred_images = einops.rearrange(pred_images, 'b y x -> b 1 y x')
     return pred_images
 
-# from cryodyna.utils.transforms import SpatialGridTranslate
-# translator = SpatialGridTranslate(D=cfg.data_process.down_side_shape)
-# rotmats = torch.split(rotmats,500)
-# trans = torch.split(torch.from_numpy(trans),500)
-# target_images = []
-# for rotmat_, tran_ in zip(rotmats, trans):
-#     target_image = _shared_projection(torch.tensor(meta.coord)[None,...], rotmat_, ref_sigmas,ref_amps,grid,mask_gmm)
-#     target_image = translator.transform(einops.rearrange(target_image, "B 1 NY NX -> B NY NX"),
-#                                                   einops.rearrange(tran_, "B C2 -> B 1 C2"))
-#     target_images.append(target_image.detach())
+# 以star pose+ref pdb制作target_image
+from cryodyna.utils.transforms import SpatialGridTranslate
+translator = SpatialGridTranslate(D=cfg.data_process.down_side_shape)
+rotmats = torch.split(rotmats,500)
+trans = torch.split(torch.from_numpy(trans),500)
+target_images = []
+for rotmat_, tran_ in zip(rotmats, trans):
+    target_image = _shared_projection(torch.tensor(meta.coord)[None,...], rotmat_, ref_sigmas,ref_amps,grid,mask_gmm)
+    target_image = translator.transform(einops.rearrange(target_image, "B 1 NY NX -> B NY NX"),
+                                                  einops.rearrange(tran_, "B C2 -> B 1 C2"))
+    target_images.append(target_image.detach())
     
-# target_images = torch.cat(target_images,dim=0)
-target_images = np.load('./test_pose/target_images.npy')
+target_images = torch.cat(target_images,dim=0)
+# target_images = np.load('./test_pose/target_images.npy')
+
 from torch.utils.data import Dataset, DataLoader
 class PoseOptimizationDataset(Dataset):
     def __init__(
@@ -309,6 +313,11 @@ iter_num = 50
 translator = SpatialGridTranslate(D=cfg.data_process.down_side_shape,device=device)
 # optimizer = optim.Adam([{'params':pose_params, 'lr':1e-3}])
 update_pose = []
+
+# 提前开好文件夹
+from pathlib import Path
+Path("test_pose").mkdir(parents=True, exist_ok=True)
+
 for batch_idx, batch in enumerate(dataloader):
         target_images,deformed_gmm, initial_poses = batch['target_image'],batch['deformed_gmm'],batch['initial_pose']
         target_images,deformed_gmm,initial_poses = target_images.to(device), deformed_gmm.to(device), initial_poses.to(device)
@@ -332,5 +341,5 @@ for batch_idx, batch in enumerate(dataloader):
                 opt.step()
                 print(f"Batch  {batch_idx:3d}   Iter {it:3d}: Loss = {loss.detach().item():.4f}")
         update_pose.append(pose.detach().cpu().numpy())
-        
+
 np.save('./test_pose/update_pose_mask_sigma.npy',np.vstack(update_pose))
